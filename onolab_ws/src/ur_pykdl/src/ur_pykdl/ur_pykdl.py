60303b044f8e3186ffa5f86ff7ec10012216d0b4
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-06-13 00:54:52 +0000

    Fix for ros2

--- onolab_ws/src/ur_pykdl/src/ur_pykdl/ur_pykdl.py
+++ onolab_ws/src/ur_pykdl/src/ur_pykdl/ur_pykdl.py
@@ -30,46 +30,55 @@
 import numpy as np
 import PyKDL
 
-import rospy
-import rospkg
-
 from ur_kdl.kdl_parser import kdl_tree_from_urdf_model
 from urdf_parser_py.urdf import URDF
 from ur_control import transformations
 
 # Set constants for joints
-SHOULDER_PAN_JOINT = 'shoulder_pan_joint'
-SHOULDER_LIFT_JOINT = 'shoulder_lift_joint'
-ELBOW_JOINT = 'elbow_joint'
-WRIST_1_JOINT = 'wrist_1_joint'
-WRIST_2_JOINT = 'wrist_2_joint'
-WRIST_3_JOINT = 'wrist_3_joint'
+SHOULDER_PAN_JOINT = "shoulder_pan_joint"
+SHOULDER_LIFT_JOINT = "shoulder_lift_joint"
+ELBOW_JOINT = "elbow_joint"
+WRIST_1_JOINT = "wrist_1_joint"
+WRIST_2_JOINT = "wrist_2_joint"
+WRIST_3_JOINT = "wrist_3_joint"
 
 # Set constants for links
-BASE_LINK = 'base_link'
-SHOULDER_LINK = 'shoulder_link'
-UPPER_ARM_LINK = 'upper_arm_link'
-FOREARM_LINK = 'forearm_link'
-WRIST_1_LINK = 'wrist_1_link'
-WRIST_2_LINK = 'wrist_2_link'
-WRIST_3_LINK = 'wrist_3_link'
-EE_LINK = 'ur3_robotiq_85_gripper'
-EE_LINK = 'ee_link'
+BASE_LINK = "base_link"
+SHOULDER_LINK = "shoulder_link"
+UPPER_ARM_LINK = "upper_arm_link"
+FOREARM_LINK = "forearm_link"
+WRIST_1_LINK = "wrist_1_link"
+WRIST_2_LINK = "wrist_2_link"
+WRIST_3_LINK = "wrist_3_link"
+EE_LINK = "ur3_robotiq_85_gripper"
+EE_LINK = "ee_link"
 
 # Only edit these when editing the robot joints and links.
 # The lengths of these arrays define numerous parameters in GPS.
-JOINT_ORDER = [SHOULDER_PAN_JOINT, SHOULDER_LIFT_JOINT, ELBOW_JOINT,
-               WRIST_1_JOINT, WRIST_2_JOINT, WRIST_3_JOINT]
-LINK_NAMES = [BASE_LINK, SHOULDER_LINK, UPPER_ARM_LINK, FOREARM_LINK,
-              WRIST_1_LINK, WRIST_2_LINK, WRIST_3_LINK]
+JOINT_ORDER = [
+    SHOULDER_PAN_JOINT,
+    SHOULDER_LIFT_JOINT,
+    ELBOW_JOINT,
+    WRIST_1_JOINT,
+    WRIST_2_JOINT,
+    WRIST_3_JOINT,
+]
+LINK_NAMES = [
+    BASE_LINK,
+    SHOULDER_LINK,
+    UPPER_ARM_LINK,
+    FOREARM_LINK,
+    WRIST_1_LINK,
+    WRIST_2_LINK,
+    WRIST_3_LINK,
+]
 
 
 def frame_to_list(frame):
     pos = frame.p
     rot = PyKDL.Rotation(frame.M)
     rot = rot.GetQuaternion()
-    return np.array([pos[0], pos[1], pos[2],
-                     rot[0], rot[1], rot[2], rot[3]])
+    return np.array([pos[0], pos[1], pos[2], rot[0], rot[1], rot[2], rot[3]])
 
 
 class ur_kinematics(object):
@@ -77,12 +86,14 @@ class ur_kinematics(object):
     UR Kinematics with PyKDL
     """
 
-    def __init__(self, base_link=None, ee_link=None, robot=None, prefix=None, rospackage=None):
+    def __init__(
+        self, base_link=None, ee_link=None, robot=None, prefix=None, rospackage=None
+    ):
         if robot:
             rospack = rospkg.RosPack()
-            rospackage_ = rospackage if rospackage is not None else 'ur_pykdl'
+            rospackage_ = rospackage if rospackage is not None else "ur_pykdl"
             pykdl_dir = rospack.get_path(rospackage_)
-            TREE_PATH = pykdl_dir + '/urdf/' + robot + '.urdf'
+            TREE_PATH = pykdl_dir + "/urdf/" + robot + ".urdf"
             self._ur = URDF.from_xml_file(TREE_PATH)
         else:
             self._ur = URDF.from_parameter_server()
@@ -92,28 +103,28 @@ class ur_kinematics(object):
 
         self._tip_link = EE_LINK if ee_link is None else ee_link
         self._tip_frame = PyKDL.Frame()
-        self._arm_chain = self._kdl_tree.getChain(self._base_link,
-                                                  self._tip_link)
+        self._arm_chain = self._kdl_tree.getChain(self._base_link, self._tip_link)
 
         # UR Interface Limb Instances
-        self._joint_names = JOINT_ORDER if prefix is None else [prefix + joint for joint in JOINT_ORDER]
+        self._joint_names = (
+            JOINT_ORDER if prefix is None else [prefix + joint for joint in JOINT_ORDER]
+        )
         self._num_jnts = len(self._joint_names)
 
         # KDL Solvers
         self._fk_p_kdl = PyKDL.ChainFkSolverPos_recursive(self._arm_chain)
         self._fk_v_kdl = PyKDL.ChainFkSolverVel_recursive(self._arm_chain)
         self._ik_v_kdl = PyKDL.ChainIkSolverVel_pinv(self._arm_chain)
-        self._ik_p_kdl = PyKDL.ChainIkSolverPos_NR(self._arm_chain,
-                                                   self._fk_p_kdl,
-                                                   self._ik_v_kdl)
+        self._ik_p_kdl = PyKDL.ChainIkSolverPos_NR(
+            self._arm_chain, self._fk_p_kdl, self._ik_v_kdl
+        )
         self._jac_kdl = PyKDL.ChainJntToJacSolver(self._arm_chain)
-        self._dyn_kdl = PyKDL.ChainDynParam(self._arm_chain,
-                                            PyKDL.Vector.Zero())
+        self._dyn_kdl = PyKDL.ChainDynParam(self._arm_chain, PyKDL.Vector.Zero())
 
     def print_robot_description(self):
         nf_joints = 0
         for j in self._ur.joints:
-            if j.type != 'fixed':
+            if j.type != "fixed":
                 nf_joints += 1
         print(("URDF non-fixed joints: %d;" % nf_joints))
         print(("URDF total joints: %d" % len(self._ur.joints)))
@@ -123,7 +134,7 @@ class ur_kinematics(object):
 
     def print_kdl_chain(self):
         for idx in range(self._arm_chain.getNrOfSegments()):
-            print(('* ' + self._arm_chain.getSegment(idx).getName()))
+            print(("* " + self._arm_chain.getSegment(idx).getName()))
 
     def joints_to_kdl(self, type, values):
         kdl_array = PyKDL.JntArray(self._num_jnts)
@@ -132,7 +143,7 @@ class ur_kinematics(object):
 
         for idx in range(self._num_jnts):
             kdl_array[idx] = cur_type_values[idx]
-        if type == 'velocities':
+        if type == "velocities":
             kdl_array = PyKDL.JntArrayVel(kdl_array)
         return kdl_array
 
@@ -154,24 +165,24 @@ class ur_kinematics(object):
         if not tip_link or tip_link == self._tip_link:
             return self.forward_position_kinematics(joint_values)
 
-        arm_chain = self._kdl_tree.getChain(self._base_link,
-                                            tip_link)
+        arm_chain = self._kdl_tree.getChain(self._base_link, tip_link)
         fk_p_kdl = PyKDL.ChainFkSolverPos_recursive(arm_chain)
         end_frame = PyKDL.Frame()
-        fk_p_kdl.JntToCart(self.joints_to_kdl('positions', joint_values),
-                           end_frame)
+        fk_p_kdl.JntToCart(self.joints_to_kdl("positions", joint_values), end_frame)
         return frame_to_list(end_frame)
 
     def forward_position_kinematics(self, joint_values):
         end_frame = PyKDL.Frame()
-        self._fk_p_kdl.JntToCart(self.joints_to_kdl('positions', joint_values),
-                                 end_frame)
+        self._fk_p_kdl.JntToCart(
+            self.joints_to_kdl("positions", joint_values), end_frame
+        )
         return frame_to_list(end_frame)
 
     def forward_velocity_kinematics(self, joint_velocities):
         end_frame = PyKDL.FrameVel()
-        self._fk_v_kdl.JntToCart(self.joints_to_kdl('velocities', joint_velocities),
-                                 end_frame)
+        self._fk_v_kdl.JntToCart(
+            self.joints_to_kdl("velocities", joint_velocities), end_frame
+        )
         return end_frame.GetTwist()
 
     def inverse_kinematics(self, position, orientation=None, seed=None):
@@ -179,8 +190,9 @@ class ur_kinematics(object):
         pos = PyKDL.Vector(position[0], position[1], position[2])
         if isinstance(orientation, (np.ndarray, np.generic, list)):
             rot = PyKDL.Rotation()
-            rot = rot.Quaternion(orientation[0], orientation[1],
-                                 orientation[2], orientation[3])
+            rot = rot.Quaternion(
+                orientation[0], orientation[1], orientation[2], orientation[3]
+            )
         # Populate seed with current angles if not provided
         seed_array = PyKDL.JntArray(self._num_jnts)
         if isinstance(seed, (np.ndarray, np.generic, list)):
@@ -188,7 +200,7 @@ class ur_kinematics(object):
             for idx, jnt in enumerate(seed):
                 seed_array[idx] = jnt
         else:
-            seed_array = self.joints_to_kdl('positions', None)  # TODO: Fixme
+            seed_array = self.joints_to_kdl("positions", None)  # TODO: Fixme
 
         # Make IK Call
         if orientation.size != 0:
@@ -205,7 +217,7 @@ class ur_kinematics(object):
 
     def jacobian(self, joint_values=None):
         jacobian = PyKDL.Jacobian(self._num_jnts)
-        self._jac_kdl.JntToJac(self.joints_to_kdl('positions', joint_values), jacobian)
+        self._jac_kdl.JntToJac(self.joints_to_kdl("positions", joint_values), jacobian)
         return self.kdl_to_mat(jacobian)
 
     def jacobian_transpose(self, joint_values=None):
@@ -216,7 +228,7 @@ class ur_kinematics(object):
 
     def inertia(self, joint_values=None):
         inertia = PyKDL.JntSpaceInertiaMatrix(self._num_jnts)
-        self._dyn_kdl.JntToMass(self.joints_to_kdl('positions', joint_values), inertia)
+        self._dyn_kdl.JntToMass(self.joints_to_kdl("positions", joint_values), inertia)
         return self.kdl_to_mat(inertia)
 
     def cart_inertia(self, joint_values=None):
