ce777e33ee2c6046216ebfb3b56f7d287436acb7
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-06-13 00:57:49 +0000

    Add build script

---
+++ onolab_ws/build_ros_ws.sh
@@ -0,0 +1,15 @@
+### Ros Workspace ###
+# Set up the workspace
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+apt update
+
+rosdep update
+
+# Updating ROSDEP and installing dependencies
+cd ~/onolab/onolab_ws && vcs import src < src/underlay.rosinstall
+rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
+
+# Build
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+source /home/ubuntu/onolab/onolab_ws/install/setup.bash
