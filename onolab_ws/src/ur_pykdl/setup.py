65768cf6284cb934e1f947c947778ef822f9eecc
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-06-13 09:25:02 +0000

    Updaet

--- onolab_ws/src/ur_pykdl/setup.py
+++ onolab_ws/src/ur_pykdl/setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup
 
-package_name = 'ur_pykdl'
+package_name = "ur_pykdl"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ubuntu',
-    maintainer_email='yusaku_nakajima@ap.eng.osaka-u.ac.jp',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="ubuntu",
+    maintainer_email="yusaku_nakajima@ap.eng.osaka-u.ac.jp",
+    description="KDL package for Universal Robot.",
+    license="BSD",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
