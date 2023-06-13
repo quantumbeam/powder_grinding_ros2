f1f4c7d1fcb1c08fdb799581a597a0556f2833bb
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-06-13 09:24:37 +0000

    Add clearn

--- onolab_ws/build_ros_ws.sh
+++ onolab_ws/build_ros_ws.sh
@@ -11,5 +11,5 @@ cd ~/onolab/onolab_ws && vcs import src < src/underlay.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
 
 # Build
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --cmake-clean-cache
 source /home/ubuntu/onolab/onolab_ws/install/setup.bash
