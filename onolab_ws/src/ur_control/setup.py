5450f39b746c07dcd6a3dc34bce7610902011f05
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-06-13 09:24:25 +0000

    Add launch and test scripts

--- onolab_ws/src/ur_control/setup.py
+++ onolab_ws/src/ur_control/setup.py
@@ -1,25 +1,30 @@
 from setuptools import setup
+import os
+from glob import glob
 
-package_name = 'ur_control'
+package_name = "ur_control"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name), glob("./launch/*.launch.py")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ubuntu',
-    maintainer_email='yusaku_nakajima@ap.eng.osaka-u.ac.jp',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="ubuntu",
+    maintainer_email="yusaku_nakajima@ap.eng.osaka-u.ac.jp",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "publisher_joint_trajectory_controller = "
+            + package_name
+            + ".publisher_joint_trajectory_controller:main",
         ],
     },
 )
