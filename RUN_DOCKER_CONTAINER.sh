b1ee1d81a277aa54282ad48a5d7fab9d3e6c9838
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-09-26 19:00:09 +0900

    Add run docker script

---
+++ RUN_DOCKER_CONTAINER.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+################################################################################
+# arg1: project name
+
+################################################################################
+
+# Set the Docker container name from a project name (first argument).
+# If no argument is given, use the current user name as the project name.
+PROJECT=$1
+if [ -z "${PROJECT}" ]; then
+  PROJECT=${USER}
+fi
+CONTAINER="${PROJECT}_onolab_1"
+echo "$0: PROJECT=${PROJECT}"
+echo "$0: CONTAINER=${CONTAINER}"
+
+# Run the Docker container in the background.
+# Any changes made to './docker/docker-compose.yml' will recreate and overwrite the container.
+docker-compose -p ${PROJECT} -f ./docker/docker-compose.yml up -d 
+
+################################################################################
+
+# Display GUI through X Server by granting full access to any external client.
+xhost +
+
+################################################################################
+
+# Enter the Docker container with a Bash shell
+docker exec -i -t ${CONTAINER} bash
