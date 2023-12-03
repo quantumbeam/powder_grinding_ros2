040d7820328883b9970635cdee19ace5af5cacd7
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-12-03 13:00:12 +0900

    Update

--- onolab_ws/BUILD_ROS_WORKSPACE.sh
+++ onolab_ws/BUILD_ROS_WORKSPACE.sh
@@ -3,9 +3,13 @@
 
 sudo apt update
 
-rosdep update
+# Downloading packages
+vcs import src < src/third_party.repos
+vcs import src/third_party/Universal_Robots < src/third_party/Universal_Robots/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
+vcs import src/third_party/MoveIt2 < src/third_party/MoveIt2/moveit2_tutorials/moveit2_tutorials.repos
 
-# Updating ROSDEP and installing dependencies
+# Updating rosdep and installing dependencies
+rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
 
 # Build
