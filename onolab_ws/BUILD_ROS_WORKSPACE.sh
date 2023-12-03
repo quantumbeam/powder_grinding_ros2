d95ea2ca69fe021287860dbac411122cd3a2e6a6
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2023-12-03 10:22:08 +0900

    Update repo file

--- onolab_ws/BUILD_ROS_WORKSPACE.sh
+++ onolab_ws/BUILD_ROS_WORKSPACE.sh
@@ -6,11 +6,10 @@ sudo apt update
 rosdep update
 
 # Updating ROSDEP and installing dependencies
-vcs import src < src/third_party.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
 
 # Build
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --cmake-clean-cache
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 # Update enviromental veriables
 source /opt/ros/$ROS_DISTRO/setup.bash
--- onolab_ws/INITIAL_SETUP_ROS_ENVIROMENTS.sh
+++ onolab_ws/INITIAL_SETUP_ROS_ENVIROMENTS.sh
@@ -8,4 +8,15 @@ sudo pip install --upgrade pip
 
 # pip3 install -r src/requirements.txt
 
-. BUILD_ROS_WORKSPACE.sh
+rosdep update
+vcs import src < src/third_party.repos
+vcs import src/third_party/Universal_Robots < src/third_party/Universal_Robots/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
+
+rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
+
+# Build
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --cmake-clean-cache
+
+# Update enviromental veriables
+source /opt/ros/$ROS_DISTRO/setup.bash
+source /home/ubuntu/onolab/onolab_ws/install/setup.bash
