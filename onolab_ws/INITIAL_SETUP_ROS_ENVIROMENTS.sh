5c6c0fd06e2866f7c38fce76fa49b834cea6a5d0
Yusaku Nakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-02-17 15:31:13 +0900

    Remove moveit2 tutorial

--- onolab_ws/INITIAL_SETUP_ROS_ENVIROMENTS.sh
+++ onolab_ws/INITIAL_SETUP_ROS_ENVIROMENTS.sh
@@ -10,7 +10,6 @@ sudo pip install --upgrade pip
 
 vcs import src < src/third_party.repos
 vcs import src/third_party/Universal_Robots < src/third_party/Universal_Robots/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
-vcs import src/third_party/MoveIt2 < src/third_party/MoveIt2/moveit2_tutorials/moveit2_tutorials.repos
 vcs pull src
 
 rosdep update
