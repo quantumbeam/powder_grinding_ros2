168b9921a7de754af446a42122ff8efbe5c6dc03
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-05-04 14:46:53 +0900

    Delete repos

--- onolab_ws/BUILD_ROS_WORKSPACE.sh
+++ onolab_ws/BUILD_ROS_WORKSPACE.sh
@@ -4,9 +4,9 @@
 sudo apt update
 
 # Downloading packages
-vcs import src < src/third_party.repos
-vcs import src/third_party/Universal_Robots < src/third_party/Universal_Robots/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
-vcs import src/third_party/MoveIt2 < src/third_party/MoveIt2/moveit2_tutorials/moveit2_tutorials.repos
+# vcs import src < src/third_party.repos
+# vcs import src/third_party/Universal_Robots < src/third_party/Universal_Robots/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
+# vcs import src/third_party/MoveIt2 < src/third_party/MoveIt2/moveit2_tutorials/moveit2_tutorials.repos
 vcs pull src
 
 # Updating rosdep and installing dependencies
--- onolab_ws/INITIAL_SETUP_ROS_ENVIROMENTS.sh
+++ onolab_ws/INITIAL_SETUP_ROS_ENVIROMENTS.sh
@@ -9,9 +9,9 @@ sudo pip install --upgrade pip
 # pip3 install -r src/requirements.txt
 
 rosdep update
-vcs import src < src/third_party.repos
-vcs import src/third_party/Universal_Robots < src/third_party/Universal_Robots/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
-vcs import src/third_party/MoveIt2 < src/third_party/MoveIt2/moveit2_tutorials/moveit2_tutorials.repos
+# vcs import src < src/third_party.repos
+# vcs import src/third_party/Universal_Robots < src/third_party/Universal_Robots/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.humble.repos
+# vcs import src/third_party/MoveIt2 < src/third_party/MoveIt2/moveit2_tutorials/moveit2_tutorials.repos
 
 rosdep install --from-paths src --ignore-src --rosdistro=$ROS_DISTRO -y
 
