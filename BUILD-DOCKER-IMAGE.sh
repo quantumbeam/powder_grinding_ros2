4d8276e5bf95c8ffeeee7b1e088c0276f1cc50ea
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-11-02 22:56:20 +0900

    Update for nvidia

--- BUILD-DOCKER-IMAGE.sh
+++ BUILD-DOCKER-IMAGE.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Set the Docker container name from a <user> input argument (first argument).
-# If no <user> argument is given, use the current user name as docker project name.
-DOCKER_PROJECT=$1
-if [ -z "${DOCKER_PROJECT}" ]; then
-  DOCKER_PROJECT=${USER}
-fi
-DOCKER_CONTAINER="${DOCKER_PROJECT}_onolab_ros2_humble"
+# Use the current user name as docker project name.
+DOCKER_PROJECT=${USER}
+
+NVIDIA_OPTION=$1
+echo "$1: NVIDIA_OPTION=${NVIDIA_OPTION}"
 echo "$0: USER=${DOCKER_PROJECT}"
-echo "$0: DOCKER_CONTAINER=${DOCKER_CONTAINER}"
+if [[ $NVIDIA_OPTION == *"nvidia"* ]]; then
+  DOCKER_CONTAINER="${DOCKER_PROJECT}_onolab_ros_humble_nvidia_1"
+  echo "$0: DOCKER_CONTAINER=${DOCKER_CONTAINER}"
+else
+  DOCKER_CONTAINER="${DOCKER_PROJECT}_onolab_ros_humble_1"
+  echo "$0: DOCKER_CONTAINER=${DOCKER_CONTAINER}"
+fi
 
 # Stop and remove the Docker container.
 EXISTING_DOCKER_CONTAINER_ID=`docker ps -aq -f name=${DOCKER_CONTAINER}`
@@ -20,8 +24,13 @@ if [ ! -z "${EXISTING_DOCKER_CONTAINER_ID}" ]; then
 fi
 
 # Build docker image
-docker compose -p ${DOCKER_PROJECT} -f docker/compose.yaml build
+if [[ $NVIDIA_OPTION == *"nvidia"* ]]; then
+    docker compose -p ${DOCKER_PROJECT} -f docker/with_nvidia/compose.yaml build
+else
+    docker compose -p ${DOCKER_PROJECT} -f docker/compose.yaml build
+fi
 
 # Initialize environments in the container
+echo "Finished building docker image."
 echo "You should run RUN-DOCKER-CONTAINER.sh at home directory and INITIAL_SETUP_ROS_ENVIROMENTS.sh in docker container at first."
 echo "After that, you can run ROS commands and use packages in docker container."
--- RUN-DOCKER-CONTAINER.sh
+++ RUN-DOCKER-CONTAINER.sh
@@ -8,18 +8,27 @@
 
 # Set the Docker container name from a project name (first argument).
 # If no argument is given, use the current user name as the project name.
-PROJECT=$1
-if [ -z "${PROJECT}" ]; then
-  PROJECT=${USER}
+PROJECT=${USER}
+NVIDIA_OPTION=$1
+if [[ $NVIDIA_OPTION == *"nvidia"* ]]; then
+  CONTAINER="${PROJECT}-onolab-ros-humble-nvidia-1"
+else
+  CONTAINER="${PROJECT}-onolab-ros-humble-1"
 fi
-CONTAINER="${PROJECT}-humble-1"
-echo "$0: USER=${USER}"
 echo "$0: PROJECT=${PROJECT}"
 echo "$0: CONTAINER=${CONTAINER}"
+echo $(dirname "$0")/docker/compose.yaml
+echo "$1: NVIDIA_OPTION=${NVIDIA_OPTION}"
+
 
 # Run the Docker container in the background.
-# Any changes made to './docker/docker-compose.yml' will recreate and overwrite the container.
-docker compose -p ${PROJECT} -f ./docker/compose.yaml up -d --remove-orphans
+# Any changes made to './docker/compose.yaml' will recreate and overwrite the container.
+if [[ $NVIDIA_OPTION == *"nvidia"* ]]; then
+    docker compose -p ${PROJECT} -f $(dirname "$0")/docker/with_nvidia/compose.yaml up -d
+else
+    docker compose -p ${PROJECT} -f $(dirname "$0")/docker/compose.yaml up -d
+fi
+
 
 ################################################################################
 
