b4d5c837f399b10587a30da3e969f0e017d0cbd7
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-12-22 18:20:33 +0900

    Add config yaml

--- onolab_ws/src/grinding_scene_description/launch/load_planning_scene.launch.py
+++ onolab_ws/src/grinding_scene_description/launch/load_planning_scene.launch.py
@@ -1,22 +1,32 @@
 
-import launch
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        ComposableNodeContainer(
-            package='rclcpp_components',
-            executable='component_container',
-            name='load_scene_container',
-            namespace='',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='grinding_scene_description',
-                    plugin='grinding_scene_description::PlanningSceneLoader',
-                    name='scene_load_node'
-                )
-            ],
-            output='screen'
-        )
+        
+    config = PathJoinSubstitution(
+    [FindPackageShare("grinding_scene_description"),'config','planning_scene_config.yaml']
+    )
+    
+    load_scene_container=ComposableNodeContainer(
+        package='rclcpp_components',
+        executable='component_container',
+        name='load_scene_container',
+        namespace='',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='grinding_scene_description',
+                plugin='grinding_scene_description::PlanningSceneLoader',
+                name='scene_load_node',
+                parameters=[config]
+            )
+        ],
+        output='screen'
+    )
+        
+    return LaunchDescription([
+        load_scene_container,
     ])
