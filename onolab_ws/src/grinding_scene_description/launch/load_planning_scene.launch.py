3ba00aac8e6439c71146a69b6b370de7a68f35b2
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2024-12-23 09:46:53 +0900

    Update

--- onolab_ws/src/grinding_scene_description/launch/load_planning_scene.launch.py
+++ onolab_ws/src/grinding_scene_description/launch/load_planning_scene.launch.py
@@ -1,15 +1,19 @@
 
+import os
 from launch import LaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import ComposableNodeContainer
+# from launch.substitutions import PathJoinSubstitution
+# from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
-from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
         
-    config = PathJoinSubstitution(
-    [FindPackageShare("grinding_scene_description"),'config','planning_scene_config.yaml']
-    )
+    # planning_scene_config = PathJoinSubstitution(
+    # [FindPackageShare("grinding_scene_description"),"config","planning_scene_config.yaml"]
+    # )
+    planning_scene_config = os.path.join(get_package_share_directory('grinding_scene_description'), 'config', 'planning_scene_config.yaml')
+    print(planning_scene_config)
     
     load_scene_container=ComposableNodeContainer(
         package='rclcpp_components',
@@ -21,12 +25,20 @@ def generate_launch_description():
                 package='grinding_scene_description',
                 plugin='grinding_scene_description::PlanningSceneLoader',
                 name='scene_load_node',
-                parameters=[config]
+                parameters=[planning_scene_config]
             )
         ],
         output='screen'
     )
+    load_scene_node=Node(
+        package='grinding_scene_description',
+        executable='load_planning_scene_node',
+        name='planning_scene_loader',
+        parameters=[planning_scene_config],
+        output='screen',
+    )
         
     return LaunchDescription([
-        load_scene_container,
+        # load_scene_container,
+        load_scene_node
     ])
