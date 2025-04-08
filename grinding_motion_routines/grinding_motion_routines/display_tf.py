86e953f2eab6bf4f67faed37da9818170c5b636e
Kai Kawasaki <kai_kawasaki@ap.eng.osaka-u.ac.jp>
2025-04-08 15:17:22 +0900

    Restore display TF code

--- grinding_motion_routines/grinding_motion_routines/display_tf.py
+++ grinding_motion_routines/grinding_motion_routines/display_tf.py
@@ -1,106 +1,67 @@
-import rclpy
-from rclpy.node import Node
-from visualization_msgs.msg import Marker
-from visualization_msgs.msg import MarkerArray
+#!/usr/bin/env python3
+import rospy
+import tf2_ros
+import geometry_msgs.msg
+from geometry_msgs.msg import Pose, Quaternion
 
 
-from tf_transformations import quaternion_from_euler, quaternion_multiply
-from geometry_msgs.msg import Quaternion, Pose
-from builtin_interfaces.msg import Duration
-import numpy as np
+class TFPublisher:
+    def __init__(self) -> None:
+        self.broadcaster = tf2_ros.TransformBroadcaster()
+        self.tf = geometry_msgs.msg.TransformStamped()
+        
+        pass
 
+    def broadcast_tf_with_waypoints(
+        self, waypoints, parent_link="base_link", child_link="debug_"
+    ):
+        rate = rospy.Rate(10)
+        for index, pose in enumerate(waypoints):
+            pub_pose = Pose()
+            pub_pose.position.x = pose[0]
+            pub_pose.position.y = pose[1]
+            pub_pose.position.z = pose[2]
+            pub_pose.orientation = Quaternion(pose[3], pose[4], pose[5], pose[6])
 
-class MarkerDisplay(Node):
-    def __init__(self, marker_publisher_name):
-        super().__init__("marker_display")
-        self.publisher = self.create_publisher(MarkerArray, marker_publisher_name, 10)
-        self.rate = self.create_rate(10)
-        self.index = 0
+            self.breadcast_tf(
+                pub_pose.position,
+                pub_pose.orientation,
+                parent_link,
+                child_link + str(index),
+            )
+            rate.sleep()
 
-    def display_waypoints(self, waypoints, scale=0.02, type=None):
-        marker_array = MarkerArray()
-        if type is None:
-            type = Marker().SPHERE
-        for index, points in enumerate(waypoints):
-            marker = Marker()
+    def broadcast_tf_with_pose(self, pose, parent_link="base_link", child_link="debug"):
+        pub_pose = Pose()
+        pub_pose.position.x = pose[0]
+        pub_pose.position.y = pose[1]
+        pub_pose.position.z = pose[2]
+        pub_pose.orientation = Quaternion(pose[3], pose[4], pose[5], pose[6])
+        self.breadcast_tf(
+            pub_pose.position, pub_pose.orientation, parent_link, child_link
+        )
 
-            marker.header.frame_id = "world"
-            marker.header.stamp = self.get_clock().now().to_msg()
-            marker.ns = "waypoints"
-            self.index += 1
-            marker.id = self.index
-            print(marker.id)
-            marker.action = Marker.ADD
+    def breadcast_tf(self, tf_pos, tf_rot, parent_link, child_link):
+        self.tf.header.stamp = rospy.Time.now()
+        self.tf.header.frame_id = parent_link
+        self.tf.child_frame_id = child_link
+        self.tf.transform.translation = tf_pos
+        self.tf.transform.rotation = tf_rot
+        self.broadcaster.sendTransform(self.tf)
 
-            marker.pose.position = points.position
-            marker.pose.orientation = points.orientation
+    def listen_tf(self, child, parent):
+        tfBuffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(tfBuffer)
+        rate = rospy.Rate(10.0)
+        rate.sleep()
 
-            marker.color.r = 1.0
-            marker.color.g = 0.0
-            marker.color.b = 0.0
-            marker.color.a = 1.0
-            if type == marker.ARROW:
-                marker.scale.x = scale
-                marker.scale.y = scale * 0.1
-                marker.scale.z = scale * 0.1
-
-                quat = [
-                    marker.pose.orientation.x,
-                    marker.pose.orientation.y,
-                    marker.pose.orientation.z,
-                    marker.pose.orientation.w,
-                ]
-                q_orig = np.array(quat)
-                q_rot = quaternion_from_euler(0, np.pi / 2, 0)
-                q_new = quaternion_multiply(q_rot, q_orig)
-                marker.pose.orientation = Quaternion(
-                    x=q_new[0], y=q_new[1], z=q_new[2], w=q_new[3]
-                )
-            else:
-                marker.scale.x = scale
-                marker.scale.y = scale
-                marker.scale.z = scale
-
-            marker.type = type
-            marker.lifetime = Duration()  # existing marker for ever
-            marker_array.markers.append(marker)
-
-        self.publisher.publish(marker_array)
-
-
-def generate_spiral_waypoints(self, num_points):
-    waypoints = []
-    radius = 0.3  # 初期半径
-    theta = 0.0  # 初期角度
-    theta_increment = 2 * np.pi / num_points  # 角度の増分
-    scale = np.linspace(0, 1, num_points)
-    for num in range(num_points):
-        x = radius * np.cos(theta)
-        y = radius * np.sin(theta)
-        z = scale[num]  # 高さ
-        waypoint = Pose()
-        waypoint.position.x = x
-        waypoint.position.y = y
-        waypoint.position.z = z
-        waypoints.append(waypoint)
-
-        radius += scale[1]  # 半径を増加させて螺旋状にする
-        theta += theta_increment
-        print(z)
-    return waypoints
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    marker_display = MarkerDisplay("marker_publisher")
-    waypoints = generate_spiral_waypoints(marker_display, 100)
-    marker_display.display_waypoints(
-        waypoints
-    )  # replace 'waypoints' with your waypoints data
-    rclpy.spin(marker_display)
-    marker_display.destroy_node()
-    rclpy.shutdown()
-
-
-if __name__ == "__main__":
-    main()
+        try:
+            trans = tfBuffer.lookup_transform(child, parent, rospy.Time())
+            return trans
+        except (
+            tf2_ros.LookupException,
+            tf2_ros.ConnectivityException,
+            tf2_ros.ExtrapolationException,
+        ) as err:
+            rospy.loginfo("tf listen error%s" % err)
+            return err
