04763e3f0afbf8cc76be708683cc6c92d00a5521
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-04-13 20:07:45 +0900

    Add motion_demo.launch.py for grinding demo node configuration

---
+++ grinding_motion_routines/launch/motion_demo.launch.py
@@ -0,0 +1,22 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    pkg_share_motion = get_package_share_directory('grinding_motion_routines')
+    motion_config = os.path.join(pkg_share_motion, 'config', 'grinding_demo_params.yaml')
+    pkg_share_scene = get_package_share_directory('grinding_scene_description')
+    planning_scene_config = os.path.join(pkg_share_scene, 'config', 'planning_scene_params.yaml')
+
+    grinding_demo_node = Node(
+        package='grinding_motion_routines',
+        executable='grinding_demo',
+        name='grinding_demo_node', 
+        output='screen',
+        parameters=[motion_config,planning_scene_config],
+    )
+
+    return LaunchDescription([
+        grinding_demo_node
+    ])
