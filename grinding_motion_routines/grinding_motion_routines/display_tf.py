8de89698baa103f1cb363260d6a2819feda86644
takamoto <ryusei_takamoto@ap.eng.osaka-u.ac.jp>
2025-04-28 16:58:32 +0900

    Refactor TFPublisher class to inherit from Node

--- grinding_motion_routines/grinding_motion_routines/display_tf.py
+++ grinding_motion_routines/grinding_motion_routines/display_tf.py
@@ -1,48 +1,64 @@
 #!/usr/bin/env python3
-import rospy
+# import rospy
 import tf2_ros
 import geometry_msgs.msg
 from geometry_msgs.msg import Pose, Quaternion
+import numpy as np
+import rclpy
+from rclpy.node import Node
 
 
-class TFPublisher:
-    def __init__(self) -> None:
-        self.broadcaster = tf2_ros.TransformBroadcaster()
+class TFPublisher(Node):
+    def __init__(self,waypoints,parent_link="base_link", child_link="debug_") -> None:
+        super().__init__("tf_display")
+        self.broadcaster = tf2_ros.TransformBroadcaster(self)
         self.tf = geometry_msgs.msg.TransformStamped()
-        
-        pass
+        self.waypoints = waypoints
+        self.parent_link = parent_link
+        self.child_link = child_link
+        self.create_timer(0.1, self.broadcast_tf_with_waypoints)
+
 
     def broadcast_tf_with_waypoints(
-        self, waypoints, parent_link="base_link", child_link="debug_"
+        self
     ):
-        rate = rospy.Rate(10)
-        for index, pose in enumerate(waypoints):
+        # rate = rospy.Rate(10)
+        for index, pose in enumerate(self.waypoints):
+            print(pose)
             pub_pose = Pose()
             pub_pose.position.x = pose[0]
             pub_pose.position.y = pose[1]
             pub_pose.position.z = pose[2]
-            pub_pose.orientation = Quaternion(pose[3], pose[4], pose[5], pose[6])
+            pub_pose.orientation.x = pose[3]
+            pub_pose.orientation.y = pose[4]
+            pub_pose.orientation.z = pose[5]
+            pub_pose.orientation.w = pose[6]
 
             self.breadcast_tf(
                 pub_pose.position,
                 pub_pose.orientation,
-                parent_link,
-                child_link + str(index),
+                self.parent_link,
+                self.child_link + str(index),
             )
-            rate.sleep()
+        # rate.sleep()
+    
+            # rate.sleep()
 
     def broadcast_tf_with_pose(self, pose, parent_link="base_link", child_link="debug"):
         pub_pose = Pose()
         pub_pose.position.x = pose[0]
         pub_pose.position.y = pose[1]
         pub_pose.position.z = pose[2]
-        pub_pose.orientation = Quaternion(pose[3], pose[4], pose[5], pose[6])
+        pub_pose.orientation.x = pose[3]
+        pub_pose.orientation.y = pose[4]
+        pub_pose.orientation.z = pose[5]
+        pub_pose.orientation.w = pose[6]
         self.breadcast_tf(
             pub_pose.position, pub_pose.orientation, parent_link, child_link
         )
 
     def breadcast_tf(self, tf_pos, tf_rot, parent_link, child_link):
-        self.tf.header.stamp = rospy.Time.now()
+        self.tf.header.stamp = self.get_clock().now().to_msg()
         self.tf.header.frame_id = parent_link
         self.tf.child_frame_id = child_link
         self.tf.transform.translation = tf_pos
@@ -65,3 +81,42 @@ class TFPublisher:
         ) as err:
             rospy.loginfo("tf listen error%s" % err)
             return err
+        
+def generate_spiral_waypoints(num_points):
+    waypoints = []
+    radius = 0.3  # 初期半径
+    theta = 0.0  # 初期角度
+    theta_increment = 2 * np.pi / num_points  # 角度の増分
+    scale = np.linspace(0, 1, num_points)
+    for num in range(num_points):
+        x = radius * np.cos(theta)
+        y = radius * np.sin(theta)
+        z = scale[num]  # 高さ
+        waypoint = [0, 0, 0, 0, 0, 0, 0]
+        waypoint[0]= x
+        waypoint[1] = y
+        waypoint[2] = z
+        waypoint[3] = 0.0
+        waypoint[4] = 0.0
+        waypoint[5] = 0.0
+        waypoint[6] = 1.0
+        waypoints.append(waypoint)
+
+        radius += scale[1]  # 半径を増加させて螺旋状にする
+        theta += theta_increment
+        # print(z)
+    # print(waypoints)
+    return waypoints
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    waypoints = generate_spiral_waypoints(10)
+    broadcaster = TFPublisher(waypoints)
+    rclpy.spin(broadcaster)
+    broadcaster.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
