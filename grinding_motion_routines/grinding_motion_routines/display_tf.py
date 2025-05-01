39111733160c5c252c51ea4b852774f2a686f3cb
takamoto <ryusei_takamoto@ap.eng.osaka-u.ac.jp>
2025-05-01 15:21:27 +0900

    Refactor TFPublisher to use Vector3 and Quaternion for pose representation in broadcast_tf_with_waypoints

--- grinding_motion_routines/grinding_motion_routines/display_tf.py
+++ grinding_motion_routines/grinding_motion_routines/display_tf.py
@@ -2,7 +2,7 @@
 # import rospy
 import tf2_ros
 import geometry_msgs.msg
-from geometry_msgs.msg import Pose, Quaternion
+from geometry_msgs.msg import Pose, Quaternion,Vector3
 import numpy as np
 import rclpy
 from rclpy.node import Node
@@ -25,18 +25,19 @@ class TFPublisher(Node):
         # rate = rospy.Rate(10)
         for index, pose in enumerate(self.waypoints):
             print(pose)
-            pub_pose = Pose()
-            pub_pose.position.x = pose[0]
-            pub_pose.position.y = pose[1]
-            pub_pose.position.z = pose[2]
-            pub_pose.orientation.x = pose[3]
-            pub_pose.orientation.y = pose[4]
-            pub_pose.orientation.z = pose[5]
-            pub_pose.orientation.w = pose[6]
+            pub_trans = Vector3()
+            pub_trans.x = pose[0]
+            pub_trans.y = pose[1]
+            pub_trans.z = pose[2]
+            pub_rot = Quaternion()
+            pub_rot.x = pose[3]
+            pub_rot.y = pose[4]
+            pub_rot.z = pose[5]
+            pub_rot.w = pose[6]
 
             self.breadcast_tf(
-                pub_pose.position,
-                pub_pose.orientation,
+                pub_trans,
+                pub_rot,
                 self.parent_link,
                 self.child_link + str(index),
             )
@@ -44,18 +45,18 @@ class TFPublisher(Node):
     
             # rate.sleep()
 
-    def broadcast_tf_with_pose(self, pose, parent_link="base_link", child_link="debug"):
-        pub_pose = Pose()
-        pub_pose.position.x = pose[0]
-        pub_pose.position.y = pose[1]
-        pub_pose.position.z = pose[2]
-        pub_pose.orientation.x = pose[3]
-        pub_pose.orientation.y = pose[4]
-        pub_pose.orientation.z = pose[5]
-        pub_pose.orientation.w = pose[6]
-        self.breadcast_tf(
-            pub_pose.position, pub_pose.orientation, parent_link, child_link
-        )
+    # def broadcast_tf_with_pose(self, pose, parent_link="base_link", child_link="debug"):
+    #     pub_pose = Pose()
+    #     pub_pose.position.x = pose[0]
+    #     pub_pose.position.y = pose[1]
+    #     pub_pose.position.z = pose[2]
+    #     pub_pose.orientation.x = pose[3]
+    #     pub_pose.orientation.y = pose[4]
+    #     pub_pose.orientation.z = pose[5]
+    #     pub_pose.orientation.w = pose[6]
+    #     self.breadcast_tf(
+    #         pub_pose.position, pub_pose.orientation, parent_link, child_link
+    #     )
 
     def breadcast_tf(self, tf_pos, tf_rot, parent_link, child_link):
         self.tf.header.stamp = self.get_clock().now().to_msg()
@@ -65,22 +66,22 @@ class TFPublisher(Node):
         self.tf.transform.rotation = tf_rot
         self.broadcaster.sendTransform(self.tf)
 
-    def listen_tf(self, child, parent):
-        tfBuffer = tf2_ros.Buffer()
-        listener = tf2_ros.TransformListener(tfBuffer)
-        rate = rospy.Rate(10.0)
-        rate.sleep()
+    # def listen_tf(self, child, parent):
+    #     tfBuffer = tf2_ros.Buffer()
+    #     listener = tf2_ros.TransformListener(tfBuffer)
+    #     rate = rospy.Rate(10.0)
+    #     rate.sleep()
 
-        try:
-            trans = tfBuffer.lookup_transform(child, parent, rospy.Time())
-            return trans
-        except (
-            tf2_ros.LookupException,
-            tf2_ros.ConnectivityException,
-            tf2_ros.ExtrapolationException,
-        ) as err:
-            rospy.loginfo("tf listen error%s" % err)
-            return err
+    #     try:
+    #         trans = tfBuffer.lookup_transform(child, parent, rospy.Time())
+    #         return trans
+    #     except (
+    #         tf2_ros.LookupException,
+    #         tf2_ros.ConnectivityException,
+    #         tf2_ros.ExtrapolationException,
+    #     ) as err:
+    #         rospy.loginfo("tf listen error%s" % err)
+    #         return err
         
 def generate_spiral_waypoints(num_points):
     waypoints = []
