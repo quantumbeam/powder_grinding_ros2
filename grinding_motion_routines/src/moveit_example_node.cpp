972b69f1b8b55d3c1206ab965cc337971818c596
nishihori <yuki_nishihori@ap.eng.osaka-u.ac.jp>
2025-05-08 18:09:27 +0900

    add MoveIt motion

---
+++ grinding_motion_routines/src/moveit_example_node.cpp
@@ -0,0 +1,38 @@
+
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions node_options;
+  auto node = rclcpp::Node::make_shared("moveit_example_node", node_options);
+
+  moveit::planning_interface::MoveGroupInterface move_group(node, "ur_manipulator");
+
+  geometry_msgs::msg::Pose target_pose;
+  target_pose.orientation.w = 1.0;
+  target_pose.position.x = 0.28;
+  target_pose.position.y = -0.2;
+  target_pose.position.z = 0.5;
+
+  move_group.setPoseTarget(target_pose);
+
+  moveit::planning_interface::MoveGroupInterface::Plan plan;
+  auto result = move_group.plan(plan);
+
+  if (result == moveit::core::MoveItErrorCode::SUCCESS)
+  {
+    RCLCPP_INFO(node->get_logger(), "Plan successful, executing...");
+    move_group.execute(plan);
+  }
+  else
+  {
+    RCLCPP_ERROR(node->get_logger(), "Plan failed.");
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
