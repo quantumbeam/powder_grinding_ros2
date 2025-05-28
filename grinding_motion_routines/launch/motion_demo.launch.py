a01c53842d25b90ac584a608c87546836f62a785
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-05-28 18:11:31 +0900

    Fix

--- grinding_motion_routines/launch/motion_demo.launch.py
+++ grinding_motion_routines/launch/motion_demo.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     pkg_share_motion = get_package_share_directory('grinding_motion_routines')
     motion_config = os.path.join(pkg_share_motion, 'config', 'motion_config.yaml')
     pkg_share_scene = get_package_share_directory('grinding_scene_description')
-    planning_scene_config = os.path.join(pkg_share_scene, 'config', 'planning_scene_params.yaml')
+    planning_scene_config = os.path.join(pkg_share_scene, 'config', 'planning_scene_config.yaml')
 
     grinding_demo_node = Node(
         package='grinding_motion_routines',
