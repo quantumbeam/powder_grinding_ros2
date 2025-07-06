6277dbdfd9d2c79b29a17dc0498aa382b29ec572
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-07-06 12:06:57 +0900

    Refactor load_planning_scene launch file to use PathJoinSubstitution for improved path handling

--- grinding_scene_description/launch/load_planning_scene.launch.py
+++ grinding_scene_description/launch/load_planning_scene.launch.py
@@ -1,44 +1,48 @@
-
 import os
 from launch import LaunchDescription
+
 # from launch.substitutions import PathJoinSubstitution
 # from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
+from launch.substitutions import PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-        
-    # planning_scene_config = PathJoinSubstitution(
-    # [FindPackageShare("grinding_scene_description"),"config","planning_scene_config.yaml"]
-    # )
-    planning_scene_config = os.path.join(get_package_share_directory('grinding_scene_description'), 'config', 'planning_scene_config.yaml')
-    print(planning_scene_config)
-    
-    load_scene_container=ComposableNodeContainer(
-        package='rclcpp_components',
-        executable='component_container',
-        name='load_scene_container',
-        namespace='',
+    planning_scene_config = PathJoinSubstitution(
+        [
+            get_package_share_directory("grinding_scene_description"),
+            "config",
+            "planning_scene_config.yaml",
+        ]
+    )
+
+    load_scene_container = ComposableNodeContainer(
+        package="rclcpp_components",
+        executable="component_container",
+        name="load_scene_container",
+        namespace="",
         composable_node_descriptions=[
             ComposableNode(
-                package='grinding_scene_description',
-                plugin='grinding_scene_description::PlanningSceneLoader',
-                name='scene_load_node',
-                parameters=[planning_scene_config]
+                package="grinding_scene_description",
+                plugin="grinding_scene_description::PlanningSceneLoader",
+                name="scene_load_node",
+                parameters=[planning_scene_config],
             )
         ],
-        output='screen'
+        output="screen",
     )
-    load_scene_node=Node(
-        package='grinding_scene_description',
-        executable='load_planning_scene_node',
-        name='planning_scene_loader',
+    load_scene_node = Node(
+        package="grinding_scene_description",
+        executable="load_planning_scene_node",
+        name="planning_scene_loader",
         parameters=[planning_scene_config],
-        output='screen',
+        output="screen",
     )
-        
-    return LaunchDescription([
-        # load_scene_container,
-        load_scene_node
-    ])
+
+    return LaunchDescription(
+        [
+            # load_scene_container,
+            load_scene_node
+        ]
+    )
