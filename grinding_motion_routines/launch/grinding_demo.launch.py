44950df59acf332f35c016ba847b2ba4d52fd047
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-07-06 14:52:45 +0900

    Update grindign demo

--- grinding_motion_routines/launch/grinding_demo.launch.py
+++ grinding_motion_routines/launch/grinding_demo.launch.py
@@ -2,19 +2,26 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
-    pkg_share_motion = get_package_share_directory('grinding_motion_routines')
-    motion_config = os.path.join(pkg_share_motion, 'config', 'motion_config.yaml')
-    pkg_share_scene = get_package_share_directory('grinding_scene_description')
-    planning_scene_config = os.path.join(pkg_share_scene, 'config', 'planning_scene_config.yaml')
-
+    motion_config=PathJoinSubstitution([
+            get_package_share_directory("grinding_motion_routines"),
+            "config",
+            "motion_config.yaml"
+        ])
+    planning_scene_config=PathJoinSubstitution([
+            get_package_share_directory("grinding_scene_description"),
+            "config",
+            "planning_scene_config.yaml"
+        ])
+    
     grinding_demo_node = Node(
         package='grinding_motion_routines',
         executable='grinding_demo.py',
         name='grinding_demo_node', 
         output='screen',
-        prefix='terminator -x',
+        prefix='xterm -e',
         parameters=[motion_config,planning_scene_config],
     )
 
